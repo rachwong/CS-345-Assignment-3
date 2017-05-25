e667e29cc271fb8fc75941e4c8b351196a1c0e9d
rachwong <rachwong.xx@gmail.com>
2017-05-25 23:38:03 +1200

    Updating to link to out of scope
    Implemented feedback and submit assignment page. Also linked the links to go out of scope.

--- dashboard.js
+++ dashboard.js
@@ -3,10 +3,12 @@ document.addEventListener("DOMContentLoaded", function(){
     // Get the submit button from the html by its id
     var btnupload = document.getElementById("btnup");
     var btnsubmit = document.getElementById("btnsub");
+    var btnfeedback = document.getElementById("btnfb");
 
     // When the submit button is clicked, execute myFunction
     btnupload.addEventListener("click", upload);
     btnsubmit.addEventListener("click", submit);
+    btnfeedback.addEventListener("click", feedback);
 
     function upload(){
         // This function is our event handler. An alert shows when the submit button is clicked.
@@ -16,4 +18,9 @@ document.addEventListener("DOMContentLoaded", function(){
         // This function is our event handler. An alert shows when the submit button is clicked.
         alert("Assignment Submitted!");
     }
+    function feedback(){
+        // This function is our event handler. An alert shows when the submit button is clicked.
+        alert("Feedback Submitted!");
+    }
+
 });
