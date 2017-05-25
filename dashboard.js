cbbf0445a8d22a22de2228c343a869d98749b0a3
rachwong <rachwong.xx@gmail.com>
2017-05-25 22:34:08 +1200

    Create more files
    Created more pages for my website, feedback and finished submission page.
    Also added out of scope page
    
    Lol need to commmit more maybe

--- dashboard.js
+++ dashboard.js
@@ -1,19 +1,19 @@
 // Only add the listener after the html document is fully loaded
 document.addEventListener("DOMContentLoaded", function(){
     // Get the submit button from the html by its id
-    var myButton = document.getElementById("myBtn");
-    var primaryButton = document.getElementsByClassName("btn btn-primary");
+    var btnupload = document.getElementById("btnup");
+    var btnsubmit = document.getElementById("btnsub");
 
     // When the submit button is clicked, execute myFunction
-    myButton.addEventListener("click", myFunction);
-    primaryButton.addEventListener("click", primaryFunction);
+    btnupload.addEventListener("click", upload);
+    btnsubmit.addEventListener("click", submit);
 
-    function myFunction(){
-        // This function is our event handler. An alert shows when the submit button is clicked.
-        alert("Assignment Submitted!");
-    }
-    function primaryFunction(){
+    function upload(){
         // This function is our event handler. An alert shows when the submit button is clicked.
         alert("Upload Complete");
+    }
+    function submit(){
+        // This function is our event handler. An alert shows when the submit button is clicked.
+        alert("Assignment Submitted!");
     }
 });
